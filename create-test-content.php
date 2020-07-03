7d05e2007e09e138b4ff00d4ec0ba239a6a8f4e4
Igo Gomes <contato@igogomes.com.br>
2020-07-03 01:24:26 -0300

    Correção de título para página de cadastro de provas

--- create-test-content.php
+++ create-test-content.php
@@ -79,7 +79,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width initial-scale=1.0">
-    <title>Cadastrar Exercício | Station21</title>
+    <title>Cadastrar Prova | Station21</title>
     <!-- GLOBAL MAINLY STYLES-->
     <link href="./assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
     <link href="./assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet" />
