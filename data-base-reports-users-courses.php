11da1540220d2f136c5121ece14f7e6406586f94
Igo Gomes <contato@igogomes.com.br>
2020-07-11 22:10:40 -0300

    Correção na exibição de dados presentes nos relatórios individuais de desempenho de usuários

--- data-base-reports-users-courses.php
+++ data-base-reports-users-courses.php
@@ -21,7 +21,7 @@
                     <div class="form-group row">
                         <label class="col-sm-2 col-form-label">Nome</label>
                         <div class="col-sm-10">
-                            <?php echo $nome_usuario; ?>
+                            <?php echo utf8_encode($nome_usuario); ?>
                         </div>
                     </div>
                     <div class="form-group row" style="margin-bottom: 30px;">
